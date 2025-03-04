49fe02f2926e3328d34752096029fdcffff86674
Emmanuel <165190555+Emmanuel1037@users.noreply.github.com>
2025-03-04 13:46:03 -0500

    Add files via upload

--- functions/asma/api/stripe/webhook.js
+++ functions/asma/api/stripe/webhook.js
@@ -1,10 +1,8 @@
 
 export const onRequestPost = async (context) => {
 
-    const stripe = Stripe(context.env.STRIPE_SECRET)
     const endpointSecret = context.env.STRIPE_WH_SECRET
 
-    console.log(context)
-    console.log(context.request)
-    return new Response(JSON.stringify({ content: `Connection successful! from ${context.env.STRIPE_WH_SECRET}`}), { status: 205, headers: {"Content-Type": "application/json"}})
+    console.log({full: `${context}`, body: `${context.request}`})
+    return new Response(JSON.stringify({ content: `Connection successful! from ${endpointSecret}`}), { status: 205, headers: {"Content-Type": "application/json"}})
 }
