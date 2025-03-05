efe4b645d1b60607bd82261f62df9dcc907d1d60
Emmanuel <165190555+Emmanuel1037@users.noreply.github.com>
2025-03-04 20:24:13 -0500

    Add files via upload

--- functions/asma/api/stripe/webhook.js
+++ functions/asma/api/stripe/webhook.js
@@ -1,9 +1,54 @@
 
 export const onRequest = (context) => {
 
-    console.log({fullRequest: `${context}`, requestBody: `${context.request}`, environment: `${context.env}`});
+    //console.log({fullRequest: `${context}`, requestBody: `${context.request}`, environment: `${context.env}`});
 
     const endpointSecret = context.env.STRIPE_WH_SECRET
+    const IP = context.request.headers.x-real-ip
+    //some verification code
 
-    return new Response(JSON.stringify({ content: `Connection successful! from ${endpointSecret}`}), { status: 202, headers: {"Content-Type": "application/json"}})
+    const event = context.request.body.type
+    const info = context.request.body.data.object
+    const stripeID = info.customer
+
+
+    console.log({IP: `${IP}`, sss: `${endpointSecret}`, event: `${event}`, data: `${info}`})
+
+    switch (event){
+        case "checkout.session.completed":
+            const asmaID = info.client_reference_id //csc
+            const email = info.customer_details.email //csc
+            const name = info.customer_details.name //csc
+            const type = info.allow_promotion_codes //csc
+
+            if (type){
+                const update = "hands free access" 
+            }else{
+                const update = "metadata button access"
+            }
+
+            console.log({asmaID: `${asmaID}`, stripeID: `${stripeID}`, name: `${name}`, email: `${email}`, thingToUpdate: `${update}`})
+            break
+
+        case "customer.subscription.deleted":
+            const product = info.plan.product //csd
+            const update = ""
+
+            if (product === context.env.PRODUCT_1){
+                let update = "metadata button access"
+            }else if (product === context.env.PRODUCT_2){
+                let update = "hands free access"
+            }else{
+                console.log(`Unexpected product ${product}`)
+            }
+
+            console.log({stripeID: `${stripeID}`, thingToUpdate: `${update}`})
+            break
+
+        default:
+            console.log(`Unexpected event type ${event}.`)
+    }
+
+   
+    return new Response({ status: 205})
 }
