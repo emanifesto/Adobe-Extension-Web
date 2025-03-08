b16431e3cc6ee872dfca2ae4613d0f33850264b4
Emmanuel <165190555+Emmanuel1037@users.noreply.github.com>
2025-03-07 20:14:37 -0500

    Add files via upload

--- functions/asma/api/user.js
+++ functions/asma/api/user.js
@@ -1,18 +1,28 @@
 export const onRequestPost = async ({request, env}) => {
 
+    const extensionID = env.EXTENSION_ID
+    const origin = request.headers.get('origin')
     const body = await request.json()
 
-    
+    if (extensionID !== origin){
+        return new Response({status: 400})
+    }
+
+    if (body.info !== "payment"){
+        return new Response({status: 400})
+    }
+
     const auth = request.headers.get('Authorization')
     const asmaID = auth.split(' ')[1]
 
+    console.log(`${extensionID} ${origin}`)
     console.log(asmaID)
-    console.log(body)
+    console.log(body.info)
     const query = env.DB.prepare(`SELECT 1 FROM users WHERE asma_id = "${asmaID}"`)
     const data = await query.run()
 
     console.log(data)
 
 
-    return new Response(null)
+    return new Response("null")
 }
