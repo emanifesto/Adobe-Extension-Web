7fc3fcdb29ea2a5e6ef929271c608a0bfae58a70
Emmanuel <165190555+Emmanuel1037@users.noreply.github.com>
2025-03-08 21:55:56 -0500

    Add files via upload

--- functions/asma/api/feedback.js
+++ functions/asma/api/feedback.js
@@ -15,14 +15,28 @@ export const onRequestPost = async({request, env}) => {
     const query = env.DB.prepare(`SELECT * FROM users WHERE asma_id = "${asmaID}"`)
     const data = await query.run()
 
-    if (!data.results[0]){
-        return new Response(JSON.stringify({status: 400}))
-    }
+    // if (!data.results[0]){
+    //     return new Response(JSON.stringify({status: 400}))
+    // }
 
     const email = body.email
     const subject = body.subject
     const feedback = body.feedback
 
-    console.log(`${email} ${subject} ${feedback} ${data.results[0].stripe_id}`)
-    return new Response({status: 200})
+    const emailResponse = await fetch('https://api.resend.com/emails', {
+        method: "POST",
+        headers: {"Authorization": `Bearer ${env.RESEND_API_KEY}`, "Content-Type": "application/json"},
+        body: JSON.stringify({
+            from: `${env.ASMA_EMAIL}`,
+            to: `${env.PERSONAL_EMAIL}`,
+            subject: `${subject}`,
+            text: `Feedback: ${feedback}\n\nCustomer email: ${email}\n\nDatabase entry:\n${data.results[0]}`
+        })
+    })
+
+    if (emailResponse.ok){
+        return new Response({status: 200})
+    }else{
+        return new Response({status: 400})
+    }
 }
