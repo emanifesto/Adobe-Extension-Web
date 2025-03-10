201ce5fb9b76755f5d0b6681cb0e5b9a844310eb
Emmanuel <165190555+Emmanuel1037@users.noreply.github.com>
2025-03-10 15:24:04 -0400

    Add files via upload

--- functions/asma/pricing.js
+++ functions/asma/pricing.js
@@ -37,11 +37,11 @@ export async function onRequest({request, env}){
                 headers: new Headers({"Authorization": `Bearer ${STRIPE_API_KEY}`, "Content-Type": 'application/x-www-form-urlencoded'}),
                 body: new URLSearchParams({
                     customer: `${customer}`,
-                    components: JSON.stringify({
-                        pricing_table: {
-                            enabled: true,
+                    components: {
+                        "pricing_table": {
+                            "enabled": true,
                         },
-                    }),
+                    },
                 }),
             })
             console.log(customerSession)
