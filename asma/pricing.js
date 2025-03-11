18e62f891e36a5ee1b0f14746189b53d403365fe
Emmanuel <165190555+Emmanuel1037@users.noreply.github.com>
2025-03-11 14:43:47 -0400

    Add files via upload

--- asma/pricing.js
+++ asma/pricing.js
@@ -1,2 +1,11 @@
 const pricing_table = document.querySelector('stripe-pricing-table')
-Object.freeze(pricing_table)
+// Object.freeze(pricing_table)
+
+const changeDetector = new MutationObserver((changes) => {
+    changes.forEach(change => {
+            pricing_table.setAttribute(change.attributeName, change.oldValue)
+    })
+})
+
+setTimeout(Object.freeze(pricing_table), 10000)
+changeDetector.observe(pricing_table, {attributeFilter: ["client-referece-id", 'customer-session-client-secret'], attributeOldValue: true,})
