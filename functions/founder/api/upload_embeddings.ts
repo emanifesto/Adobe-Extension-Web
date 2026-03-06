661afa1a5c48fe69fc41043805dc1e2570edfbe6
emanifesto <emmanuelbabayemi37@gmail.com>
2026-03-06 01:17:29 -0500

    now we cook with gas

--- functions/founder/api/upload_embeddings.ts
+++ functions/founder/api/upload_embeddings.ts
@@ -8,16 +8,17 @@ export const onRequestPost = async ({request, env}: {request: Request, env: Env}
 
     const {notes} = await request.json()
     const query = `INSERT INTO Documents notes VALUES (?)`
-    const data = env.DB.prepare(query).bind(notes).run()
+    const { results } = await env.DB.prepare(query).bind(notes).run()
 
-    console.log(data)
-    return new Response('OK', {status: 200})
-} 
+    if (!results){
+        return new Response('Fail', {status: 400})
+    }
 
-    // const id = results[0].id
+    console.log(results)
+    const id = results[0].id
 
-    // return new Response(JSON.stringify({
-    //     'id': id,
-    //     'full': results[0]
-    // }), {status: 200})
-// }
+    return new Response(JSON.stringify({
+        'id': id,
+        'full': results[0]
+    }), {status: 200})
+}
