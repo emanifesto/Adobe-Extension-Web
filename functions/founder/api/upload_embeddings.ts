63a0966a6162e6734a022ea2dc0e143aa5f7ba8e
emanifesto <emmanuelbabayemi37@gmail.com>
2026-03-06 01:07:02 -0500

    the jig is up

--- functions/founder/api/upload_embeddings.ts
+++ functions/founder/api/upload_embeddings.ts
@@ -5,21 +5,22 @@ interface Env{
 }
 
 export const onRequestPost = async ({request, env}: {request: Request, env: Env}) => {
-    console.log(request)
 
     const {notes} = await request.json()
-    console.log(notes)
-    const query = `INSERT INTO Documents notes VALUES (?)`
-    const { results } = env.DB.prepare(query).bind(notes).run()
-
-    if (!results){
+    try{
+        const query = `INSERT INTO Documents notes VALUES (?)`
+        const { results } = env.DB.prepare(query).bind(notes).run()
+    }
+    catch(e){
+        console.log(e)
         return new Response('Failed', {status: 400})
     }
-
-    const id = results[0].id
-
-    return new Response(JSON.stringify({
-        'id': id,
-        'full': results[0]
-    }), {status: 200})
 }
+
+    // const id = results[0].id
+
+    // return new Response(JSON.stringify({
+    //     'id': id,
+    //     'full': results[0]
+    // }), {status: 200})
+// }
