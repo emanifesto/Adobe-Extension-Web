ae6b48f56891b4a84a3f5a0586472d600ad7f4a1
emanifesto <emmanuelbabayemi37@gmail.com>
2026-03-06 01:43:45 -0500

    tired agane

--- functions/founder/api/upload_embeddings.ts
+++ functions/founder/api/upload_embeddings.ts
@@ -7,10 +7,8 @@ interface Env{
 export const onRequestPost = async ({request, env}: {request: Request, env: Env}) => {
 
     const {notes} = await request.json()
-    const query = `INSERT INTO Documents (notes, id) VALUES (?) RETURNING *`
-    const data = await env.DB.prepare(query).bind(notes).run()
-    console.log(data)
-    const results = data.results
+    const query = `INSERT INTO Documents (notes) VALUES (?) RETURNING *`
+    const {results} = await env.DB.prepare(query).bind(notes).run()
 
     if (!results){
         return new Response('Fail', {status: 400})
@@ -19,16 +17,17 @@ export const onRequestPost = async ({request, env}: {request: Request, env: Env}
     console.log(results)
     const id = results[0].id
 
-    // const embeddings = await env.AI.run('@cf/baai/bge-base-en-v1.5', {
-    //     text: notes,
-    // })
+    const embeddings = await env.AI.run('@cf/baai/bge-base-en-v1.5', {
+        text: notes,
+    })
 
-    // if (!embeddings){
-    //     return new Response('Fail', {status: 400})
-    // }
+    if (!embeddings){
+        return new Response('Fail', {status: 400})
+    }
 
-    // console.log(embeddings)
-    // const values = embeddings.data[0]
+    const values = embeddings.data[0]
+    console.log(values)
+    console.log(embeddings)
 
     return new Response(JSON.stringify({
         'id': id,
