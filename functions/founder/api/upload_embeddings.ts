5718bf79b525725b49be81b89514ee63c4017358
emanifesto <emmanuelbabayemi37@gmail.com>
2026-03-06 01:35:15 -0500

    auto increment id?

--- functions/founder/api/upload_embeddings.ts
+++ functions/founder/api/upload_embeddings.ts
@@ -7,7 +7,7 @@ interface Env{
 export const onRequestPost = async ({request, env}: {request: Request, env: Env}) => {
 
     const {notes} = await request.json()
-    const query = `INSERT INTO Documents (notes) VALUES (?) RETURNING *`
+    const query = `INSERT INTO Documents (notes, id) VALUES (?) RETURNING *`
     const data = await env.DB.prepare(query).bind(notes).run()
     console.log(data)
     const results = data.results
@@ -19,16 +19,16 @@ export const onRequestPost = async ({request, env}: {request: Request, env: Env}
     console.log(results)
     const id = results[0].id
 
-    const embeddings = await env.AI.run('@cf/baai/bge-base-en-v1.5', {
-        text: notes,
-    })
+    // const embeddings = await env.AI.run('@cf/baai/bge-base-en-v1.5', {
+    //     text: notes,
+    // })
 
-    if (!embeddings){
-        return new Response('Fail', {status: 400})
-    }
+    // if (!embeddings){
+    //     return new Response('Fail', {status: 400})
+    // }
 
-    console.log(embeddings)
-    const values = embeddings.data[0]
+    // console.log(embeddings)
+    // const values = embeddings.data[0]
 
     return new Response(JSON.stringify({
         'id': id,
