1bcc18475dd273b11317c9481878187c28cd5729
emanifesto <emmanuelbabayemi37@gmail.com>
2026-03-13 16:00:51 -0400

    switching to flux model; what does it respond with?

--- functions/founder/api/generate.ts
+++ functions/founder/api/generate.ts
@@ -1,5 +1,5 @@
 export const onRequestGet = async ({request, env}: {request: Request, env: {AI: any}}) => {
-    const characterReference = await fetch('https://damisaas.com/assets/images/headshot.jpg')
+    // const characterReference = await fetch('https://damisaas.com/assets/images/headshot.jpg')
 
     const jobTitles = ["Software Engineer & Founder", "Java Supplemental Instructor", "AI/ML Researcher"]
     const artSytles = ["Anime", "Comic", "Pop", "Surrealism", "Cubism", "Chibi"]
@@ -10,39 +10,32 @@ export const onRequestGet = async ({request, env}: {request: Request, env: {AI: 
     const job = jobTitles[idx1]
     const style = artSytles[idx2]
 
-    const inputs = {
-        'prompt': `Using character reference, create an image of a ${job} in a unique random relevant setting. ${style} art-style.`,
-        'image': [...new Uint8Array(await characterReference.arrayBuffer())],
-        'height': 960,
-        'width': 540,
-    }
 
-    const response = await env.AI.run(
-        "@cf/runwayml/stable-diffusion-v1-5-img2img",
-        inputs
-    )
+    // return new Response(response, {
+    //   headers: {
+    //     "content-type": "image/png",
+    //   },
+    // });
 
-    return new Response(response, {
-      headers: {
-        "content-type": "image/png",
-      },
-    });
+    const form = new FormData()
+    form.append('prompt', `A male African-American ${job} in a unique random relevant setting. ${style} art-style.`)
+    form.append('width', '960')
+    form.append('height', '540')
 
-    // const form = new FormData()
-    // form.append('prompt', `An African-American ${job} in a unique random relevant setting. ${style} art-style.`)
-    // form.append('width', '960')
-    // form.append('height', '540')
+    const formResponse = new Response(form)
+    const formStream = formResponse.body
+    const formContentType = formResponse.headers.get('content-type')!
 
-    // const formResponse = new Response(form)
-    // const formStream = formResponse.body
-    // const formContentType = formResponse.headers.get('content-type')!
+    const response = await env.AI.run("@cf/black-forest-labs/flux-2-klein-9b", {
+      multipart: {
+        body: formStream,
+        contentType: formContentType
+      }
+    })
+    console.log(response)
+    console.log(await response.json())
+    console.log(Response.json(response))
 
-    // const response = await env.AI.run("@cf/black-forest-labs/flux-2-klein-9b", {
-    //   multipart: {
-    //     body: formStream,
-    //     contentType: formContentType
-    //   }
-    // })
-
+    return new Response('OK', {status: 200})
     // return Response.json(response)
 }
