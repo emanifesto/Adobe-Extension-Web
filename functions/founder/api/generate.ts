e9fcd066ac4176e4616c41f84a1fc9d7af3aafd3
emanifesto <emmanuelbabayemi37@gmail.com>
2026-03-13 15:48:29 -0400

    last chance for free model

--- functions/founder/api/generate.ts
+++ functions/founder/api/generate.ts
@@ -1,6 +1,5 @@
 export const onRequestGet = async ({request, env}: {request: Request, env: {AI: any}}) => {
     const characterReference = await fetch('https://damisaas.com/assets/images/headshot.jpg')
-    // const blob = await characterReference.blob()
 
     const jobTitles = ["Software Engineer & Founder", "Java Supplemental Instructor", "AI/ML Researcher"]
     const artSytles = ["Anime", "Comic", "Pop", "Surrealism", "Cubism", "Chibi"]
@@ -14,8 +13,8 @@ export const onRequestGet = async ({request, env}: {request: Request, env: {AI: 
     const inputs = {
         'prompt': `Using character reference, create an image of a ${job} in a unique random relevant setting. ${style} art-style.`,
         'image': [...new Uint8Array(await characterReference.arrayBuffer())],
-        width: 960,
-        height: 540,
+        'width': 960,
+        'height': 540,
     }
 
     const response = await env.AI.run(
@@ -28,4 +27,22 @@ export const onRequestGet = async ({request, env}: {request: Request, env: {AI: 
         "content-type": "image/png",
       },
     });
+
+    // const form = new FormData()
+    // form.append('prompt', `An African-American ${job} in a unique random relevant setting. ${style} art-style.`)
+    // form.append('width', '960')
+    // form.append('height', '540')
+
+    // const formResponse = new Response(form)
+    // const formStream = formResponse.body
+    // const formContentType = formResponse.headers.get('content-type')!
+
+    // const response = await env.AI.run("@cf/black-forest-labs/flux-2-klein-9b", {
+    //   multipart: {
+    //     body: formStream,
+    //     contentType: formContentType
+    //   }
+    // })
+
+    // return Response.json(response)
 }
